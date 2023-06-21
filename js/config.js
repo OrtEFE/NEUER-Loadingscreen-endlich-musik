bd3bf2e38280c31db5f4cc2650fc133065d3fe6e
OrtEFE <97741275+OrtEFE@users.noreply.github.com>
2023-06-22 01:34:47 +0200

    efe

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "images/wotrlogo01.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "TOE | The One Empire [IMP:RP]";
+var l_serverName = "TOE|The One Empire [IMP:RP]";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
